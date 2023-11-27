f7ada62e7624296dae191ab67e18729c276ffc87
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-11-27 18:25:04 +0200

    .

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,9 +1,7 @@
 for i in range(100):
     if i <10:
         print("{}{}".format(0,i), end=", ")
-    else:
-        print("{}".format(i), end=", ")
-
-        
-
-        
+    elif i == 99:
+        print(i)
+    # else:
+    #     print("{}".format(i), end=", ")
