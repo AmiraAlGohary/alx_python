7221c6573f50a97e662927b68bc8e762d08ccaac
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-11-27 18:09:47 +0200

    .

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,7 +1,2 @@
-# print("{},{}".format(i,j), end=", ")
-# print("{},{}".format(i,j))
-# print("{}{}".format(i,j))
-
-# else:
-        #print(str(i)+str(j), end=", ")
-#"{:d} Battery street, {}".format(98, "San Francisco")
+elif i == 99:
+        print(i)
