967bb43649057472cf5046b52d2e272cdadc038e
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-11-27 21:21:21 +0200

    .

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,4 +1,4 @@
 def convert_to_celsius(fahrenheit):
     Celsius = (fahrenheit - 32)*(5/9)
-    roundedCelsius = round(Celsius, 2)
+    roundedCelsius = round(Celsius, 13)
     return roundedCelsius
