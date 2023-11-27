48ae63587bcaa14be40327acd2db05a803485ddd
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-11-27 18:05:54 +0200

    .

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,11 +1,7 @@
 for i in range(10):
     for j in range(10):
-        if i == j:
-            pass
-        elif i > j:
-            pass
-        else:
-            if ,
-                print()
+        if i < j:
+            if i == 8 and j == 9:
+                print("{}{}".format(i,j))
             else:
                 print("{}{}".format(i,j), end=", ")
