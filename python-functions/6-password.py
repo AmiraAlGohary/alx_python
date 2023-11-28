ce0c3e63d419ce874787bf26757a1dab1a80fad3
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-11-28 23:12:58 +0200

    .

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -3,5 +3,40 @@ def validate_password(password):
     The password should be at least 8 characters long.
     The password should contain at least one uppercase letter, one lowercase letter, and one digit.
     The password should not contain spaces.
-    returns True if
+    returns True if the password passes all the checks, and False otherwise.
     """
+    alldigits = "0123456789"
+    allUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    allLower = "abcdefghijklmnopqrstuvwxyz"
+
+    for i in password:
+        if i in alldigits:
+            hasDigits = True
+            break
+        else:
+            hasDigits = False
+
+    for i in password:
+        if i in allUpper:
+            hasUpper = True
+            break
+        else:
+            hasUpper = False
+
+    for i in password:
+        if i in allLower:
+            hasLower = True
+            break
+        else:
+            hasLower = False
+
+    if len(password) >=8:
+        if " " not in password:
+            if hasDigits and hasUpper and hasLower:
+                return True
+            else:
+                return False
+        else:
+             return False
+    else:
+        return False
