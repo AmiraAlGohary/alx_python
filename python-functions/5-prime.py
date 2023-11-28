d304bb3ca94f2972980407303abf79ab88116941
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-11-28 23:45:18 +0200

    .

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,2 +1,12 @@
 def is_prime(number):
+    """
+    Returns True if the number is prime, and False otherwise.
+    """
+    if number < 2:
+        return False
+    elif number >=2:
+        for i in range(2, int(number**0.5)+1):
+            if number%i == 0:
+                return False
+        return True
     
