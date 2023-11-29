76775557c67d6bf0e3109905451a22a42f7175c3
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-11-29 02:50:18 +0200

    .

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,8 +1,27 @@
 def fibonacci_sequence(n):
-    if n == 0 or n == 1:
-        result = 1
-    elif n > 1:
-        result = fibonacci_sequence(n-1) + fibonacci_sequence(n-2)
+    """
+    Takes a number n as input
+    Returns a list of the first n Fibonacci numbers.
+    Return an empty list if the it is not possible to generate the Fibonacci numbers for n
+    """
+    def fibonacci_of(n):
+        if n == 0 or n == 1:
+            result = n
+        else:
+            result = fibonacci_of(n-1) + fibonacci_of(n-2)
+        return result
 
-    return result
-print(fibonacci_sequence(4))
+    resulted = []
+    if n == 0:
+        return resulted
+    elif n == 1:
+        resulted = [0]
+        return resulted
+    elif n == 2:
+        resulted = [0, 1]
+        return resulted
+    elif n > 2:
+        resulted = [0, 1]
+        for i in range(3, n+1):
+            resulted.append(fibonacci_of(i-1))
+        return resulted
