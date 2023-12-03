60da67ce637bc41520896168e599259d050831ad
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-04 00:09:56 +0200

    edit to my answer

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,9 +4,7 @@ a = 1
 b = 2
  
 print("{} + {} = {}".format(a, b, add(a,b)))
-# print("{} + {} = {}".format(a, b, add(a,b)), end="__name__ inside 0-add.py:, __name__")
 
-# print("__name__ inside 0-add.py:", __name__)
 
 if __name__ == "__main__":
     pass
