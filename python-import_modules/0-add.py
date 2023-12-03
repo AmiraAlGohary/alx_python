49f955336c86858336b8f1abc83eafeeec68baed
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-04 00:06:18 +0200

    .

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -8,4 +8,4 @@ print("{} + {} = {}".format(a, b, add(a,b)))
 print("__name__ inside 0-add.py:", __name__)
 
 if __name__ == "__main__":
-    print("This is being run directly as a script.")
+    pass
