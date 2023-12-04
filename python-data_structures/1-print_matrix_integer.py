d47cf4140c14e8b1de6bec1b91dead71b051e72a
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-04 21:21:56 +0200

    .

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,15 +1,14 @@
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in i:
-            if j == i[-1]:
-                print("{:d}".format(j), end="\n")
-            else:
-                print("{:d}".format(j), end=" ")
+    if matrix == [[]]:
+        print(" ")
+    else:
+        for i in matrix:
+            for j in i:
+                if j == i[-1]:
+                    print("{:d}".format(j), end="\n")
+                else:
+                    print("{:d}".format(j), end=" ")
 
-# matrix = [
-#     [1, 2, 3],
-#     [4, 5, 6],
-#     [7, 8, 9]
-# ]
+matrix = [[]]
 
-# print(print_matrix_integer(matrix))
+print(print_matrix_integer(matrix))
