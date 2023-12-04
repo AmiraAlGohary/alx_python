cf5b5458970afbd2d563102f9d36d2ffa73f24fd
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-04 21:14:53 +0200

    update to my answer

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,9 +2,9 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
             if j == i[-1]:
-                print("{}".format(j), end="\n")
+                print("{:d}".format(j), end="\n")
             else:
-                print("{}".format(j), end=" ")
+                print("{:d}".format(j), end=" ")
 
 # matrix = [
 #     [1, 2, 3],
