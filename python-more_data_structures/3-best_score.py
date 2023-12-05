5068a60554497b1f94cf7448b15a92d62f4a5295
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-05 23:54:12 +0200

    update to my answer

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -6,25 +6,25 @@ def best_score(a_dictionary):
     You can assume that all values are only integers
     You can assume all students have a different scores
     """
-    # if a_dictionary == {} or a_dictionary == None:
-    #     return None
-    # else:
-    #     BiggestValue = max(list(a_dictionary.values()))
-    #     for key in a_dictionary:
-    #         if a_dictionary[key] == BiggestValue:
-    #             thebest_score = key
-    #     return thebest_score
-
     if a_dictionary == {} or a_dictionary == None:
         return None
     else:
-        biggestValue = 0
-        newValue = 0
-
+        BiggestValue = max(list(a_dictionary.values()))
         for key in a_dictionary:
-            newValue =  a_dictionary[key]
-            if newValue > biggestValue:
-                biggestValue = newValue
+            if a_dictionary[key] == BiggestValue:
                 thebest_score = key
-        
         return thebest_score
+
+    # if a_dictionary == {} or a_dictionary == None:
+    #     return None
+    # else:
+    #     biggestValue = 0
+    #     newValue = 0
+
+    #     for key in a_dictionary:
+    #         newValue =  a_dictionary[key]
+    #         if newValue > biggestValue:
+    #             biggestValue = newValue
+    #             thebest_score = key
+        
+    #     return thebest_score
