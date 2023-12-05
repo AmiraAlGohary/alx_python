9c6c6c44c32cc0728af1d86eb9e7f7f94aeba1ac
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-05 23:16:29 +0200

    update to my answer

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -8,8 +8,6 @@ def update_dictionary(a_dictionary, key, value):
     If a key exists in the dictionary, the value will be replaced
     If a key doesn’t exist in the dictionary, it will be created
     """
-    if key in a_dictionary:
-        a_dictionary[key] = value
-
-    else:
-        a_dictionary[key] = value
+    a_dictionary[key] = value
+    
+    return a_dictionary
