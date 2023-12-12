c078cd7821ccb8a2d146f039d4f83315808f8315
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-13 00:50:58 +0200

    update to my answer

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -3,17 +3,24 @@ a python file for question no. 0
 """
 
 
+# def is_same_class(obj, a_class):
+#     """
+#     a function that returns True if the object is exactly an instance of the specified class ; otherwise False.
+#     """
+#     if a_class == object:
+#         return False
+#     elif type(obj) == bool:
+#         return False
+#     else:
+#         class_name = type(obj)
+#         if issubclass(class_name, a_class):
+#             return True
+#         else:
+#             return False
+
+
 def is_same_class(obj, a_class):
     """
     a function that returns True if the object is exactly an instance of the specified class ; otherwise False.
     """
-    if a_class == object:
-        return False
-    elif type(obj) == bool:
-        return False
-    else:
-        class_name = type(obj)
-        if issubclass(class_name, a_class):
-            return True
-        else:
-            return False
+    return type(obj) is a_class
