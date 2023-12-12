3a8de95e86cd0201af38ee08bfc7b34a61923b4a
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-13 01:01:06 +0200

    my answer

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,14 @@
+"""
+a python file for question no. 2
+"""
+
+def inherits_from(obj, a_class):
+    """
+    a function that returns True:
+    - if the object is an instance of a class that inherited (directly or indirectly) from the specified class;    
+    otherwise False.
+    """
+    if issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
