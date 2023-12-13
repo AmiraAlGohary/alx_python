de06603da88365ef4ea237f4a580e9e7ae0739d5
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-13 03:30:39 +0200

    update to my answer

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -2,28 +2,25 @@
 a python file for question no. 3
 """
 
-class MyMeta(type):
-    """
-    a metaclass to override the init_subclass attribute
-    """
-    def __dir__(cls):
-        all_attrs = dir(type(cls))
-        return [attribute for attribute in all_attrs if attribute != "__init_subclass__"]
+# class MyMeta(type):
+#     """
+#     a metaclass to override the init_subclass attribute
+#     """
+#     def __dir__(cls):
+#         all_attrs = dir(type(cls))
+#         return [attribute for attribute in all_attrs if attribute != "__init_subclass__"]
     
 
-class BaseGeometry(metaclass=MyMeta):
-    """
-    an empty class
-    """
-    def __init_subclass__(cls, **kwargs):
-        super().__init_subclass__(**kwargs)
-
-# def __init_subclass__(cls, **kwargs):
-#     super().__init_subclass__(**kwargs)
-
-# class BaseGeometry:
+# class BaseGeometry(metaclass=MyMeta):
 #     """
 #     an empty class
 #     """
-#     def dir(cls):
-#         return [attribute for attribute in super().dir() if attribute != "__init_subclass__"]
+#     def __init_subclass__(cls, **kwargs):
+#         super().__init_subclass__(**kwargs)
+
+
+class BaseGeometry:
+    """
+    an empty class
+    """
+    pass
