c484962b6851c66b4d1782633b78fb36dbb25aae
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-13 02:46:35 +0200

    update to my answer

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -13,11 +13,11 @@ class BaseGeometry():
     """
     a class
     """
-    def area(self, area):
+    def area(self):
         """
         Public instance method, that raises an Exception with the message: area() is not implemented
         """
-        raise Exception(area() is not implemented)
+        raise Exception("area() is not implemented")
 
 
 
