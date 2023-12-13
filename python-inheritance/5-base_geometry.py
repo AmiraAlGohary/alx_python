1499c41b46e9d1d62aefb52b4a3b3f4bc0c3923f
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-13 03:55:07 +0200

    my answer

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,25 @@
+"""
+a python file for question no. 5
+"""
+class BaseGeometry():
+    """
+    a class
+    """
+    def area(self):
+        """
+        Public instance method, that raises an Exception with the message: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Public instance method, that validates value.
+        you can assume name is always a string
+        """
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
+
+
+
