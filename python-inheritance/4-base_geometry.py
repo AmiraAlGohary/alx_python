75123f0b0295dc9e63e26d9e80857c468512e2f6
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-13 02:40:06 +0200

    my answer

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,23 @@
+"""
+a python file for question no. 3
+"""
+
+class MetaClass(object):
+    """
+    a class to override the init_subclass attribute
+    """
+    def dir(cls):
+        return [attribute for attribute in super().dir() if attribute != "__init_subclass__"]
+
+class BaseGeometry(MetaClass):
+    """
+    a class
+    """
+    def area(self):
+        """
+        Public instance method, that raises an Exception with the message: area() is not implemented
+        """
+        raise Exception(area() is not implemented)
+
+
+
