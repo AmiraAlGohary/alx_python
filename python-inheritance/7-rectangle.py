b54740700b18ed65ac4f920f8e2c3cd4975a1a10
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-14 01:25:27 +0200

    .

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,44 @@
+"""
+a python file for question no. 5
+"""
+class BaseGeometry():
+    """
+    a class
+    """
+    def area(self):
+        """
+        Public instance method, that raises an Exception with the message: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Public instance method, that validates value.
+        you can assume name is always a string
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """
+    rectangle class
+    """
+    def __init__(self, width, height):
+        """
+        init method
+        """
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        the_area = self.__height * self.__width
+        return the_area
+    
+    def __str__(self):
+        result = "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+        return result
