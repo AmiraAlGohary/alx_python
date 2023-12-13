ff9d67f4b9a2c5608b8ccd130179053aa7aedf66
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-13 02:43:37 +0200

    update to my answer

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -2,14 +2,14 @@
 a python file for question no. 3
 """
 
-class MetaClass(object):
-    """
-    a class to override the init_subclass attribute
-    """
-    def dir(cls):
-        return [attribute for attribute in super().dir() if attribute != "__init_subclass__"]
+# class MetaClass(object):
+#     """
+#     a class to override the init_subclass attribute
+#     """
+#     def dir(cls):
+#         return [attribute for attribute in super().dir() if attribute != "__init_subclass__"]
 
-class BaseGeometry(MetaClass):
+class BaseGeometry():
     """
     a class
     """
