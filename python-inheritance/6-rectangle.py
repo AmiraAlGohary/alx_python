4e2fa95e9ac8c3cf4339cb253c3f3df46debc84a
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-17 20:07:31 +0200

    update to my answer

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,25 +1,27 @@
 """
 a python file for question no. 7
 """
-class BaseGeometry():
-    """
-    a class
-    """
-    def area(self):
-        """
-        Public instance method, that raises an Exception with the message: area() is not implemented
-        """
-        raise Exception("area() is not implemented")
+# class BaseGeometry():
+#     """
+#     a class
+#     """
+#     def area(self):
+#         """
+#         Public instance method, that raises an Exception with the message: area() is not implemented
+#         """
+#         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
-        """
-        Public instance method, that validates value.
-        you can assume name is always a string
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+#     def integer_validator(self, name, value):
+#         """
+#         Public instance method, that validates value.
+#         you can assume name is always a string
+#         """
+#         if not isinstance(value, int):
+#             raise TypeError("{} must be an integer".format(name))
+#         if value <= 0:
+#             raise ValueError("{} must be greater than 0".format(name))
+
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
