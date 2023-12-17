b770df8cfdafefbc3986187aa9c6825301bd4296
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-18 00:37:29 +0200

    update to my answer

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,52 @@
+"""
+a python file for rectangle
+"""
+
+from base import Base
+
+class Rectangle(Base):
+    """
+    a Rectangle class
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        init method
+        """
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        self.__id = id
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, width):
+        self.__width = width
+    
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, height):
+        self.__height = height
+    
+    @property
+    def x(self):
+        return self.__x
+    
+    @x.setter
+    def x(self, x):
+        self.__x = x
+    
+    @property
+    def y(self):
+        return self.__y
+    
+    @y.setter
+    def y(self, y):
+        self.__y = y
