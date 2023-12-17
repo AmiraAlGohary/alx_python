c11e8a35de0f061c5360828aa4134a0e5965ee90
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-18 00:50:12 +0200

    update to my answer

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,23 +2,22 @@
 a python file for rectangle
 """
 
-# from base import Base
+from base import Base 
 
-
-class Base:
-    """
-    a Base class
-    """
-    __nb_objects = 0
-    def __init__(self, id=None):
-        """
-        init method
-        """
-        if id != None:
-            self.id = id
-        else:
-            Base.__nb_objects +=1
-            self.id = Base.__nb_objects
+# class Base:
+#     """
+#     a Base class
+#     """
+#     __nb_objects = 0
+#     def __init__(self, id=None):
+#         """
+#         init method
+#         """
+#         if id != None:
+#             self.id = id
+#         else:
+#             Base.__nb_objects +=1
+#             self.id = Base.__nb_objects
 
 class Rectangle(Base):
     """
