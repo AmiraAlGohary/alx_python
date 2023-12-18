ff5f8ded129735b551d9da2475140fb78b4714b2
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-18 20:50:14 +0200

    update to my answer

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -3,6 +3,7 @@ a python file for rectangle
 """
 
 from models.base import Base 
+# from base import Base
 
 class Rectangle(Base):
     """
@@ -139,4 +140,18 @@ class Rectangle(Base):
         """
         result = "[Rectangle] (" + str(self.id) + ") " + str(self.x) + "/" + str(self.y) + " - " + str(self.width) + "/" + str(self.height)
         return result
-    
+
+    def update(self, *args):
+        """
+        a public method that assigns an argument to each attribute
+        """
+        if len(args) >=1:
+            self.id = args[0]
+        if len(args) >=2:
+            self.__width = args[1]
+        if len(args) >=3:
+            self.__height = args[2]
+        if len(args) >=4:
+            self.__x = args[3]
+        if len(args) >=5:
+            self.__y = args[4]
