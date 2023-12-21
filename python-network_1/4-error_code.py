968c18c7c8bdc118f1cb0e5a7902c02687d7230d
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-21 20:36:04 +0200

    update to my answer

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -3,7 +3,7 @@ import sys
 
 def get_something(url):
     response = requests.get(url)
-    the_body = response.text
+    the_body = response.content
     if response.status_code >= 400:
         print("Error code: " + str(response.status_code))
     else:
