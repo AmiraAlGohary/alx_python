e0a2a16aad0fc36925560012bccd1de43ce9378f
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-21 20:11:07 +0200

    my answer

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,2 +1,13 @@
 import requests
 import sys
+
+def send_post_request(url, email):
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+    print("Your email is: " + str(response.text))
+
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+send_post_request(url, email)
