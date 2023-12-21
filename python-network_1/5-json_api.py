91e40ad927f2fd903c0845c85fb976c913b3999f
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-21 21:04:27 +0200

    my answer

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,2 +1,22 @@
 import requests
 import sys
+
+def search_user(the_letter):
+    if the_letter:
+        payload = {"q": the_letter}
+    else:
+        payload = {"q": ""}
+    reponse = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+
+    if reponse.headers['content-type'] == 'application/json':
+        data = reponse.json()
+        if data:
+            print("[{}] {}".fotmat(data["id"], data["name"]))
+        else:
+            print("No result")
+    else:
+        print("Not a valid JSON")
+
+the_letter = sys.argv[1]
+
+search_user(the_letter)
