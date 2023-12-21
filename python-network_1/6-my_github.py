e0a233615a22af3c2bd8f784cc7df759b5665913
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-21 21:44:04 +0200

    my answer

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,2 +1,14 @@
 import requests
 import sys
+
+
+def get_github_id(username, password):
+    url = f"https://api.github.com/user"
+    response = requests.get(url, auth=(username, password))
+    user_data = response.json()
+    return user_data[id]
+
+username = sys.argv[1]
+password = sys.argv[2]
+
+get_github_id(username, password)
