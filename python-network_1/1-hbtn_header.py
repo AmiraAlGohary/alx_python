72b03fc857821877b0a3ebfa1ee01bd2fe7d0c18
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2023-12-21 19:53:46 +0200

    update to my answer

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,2 +1,12 @@
 import requests
 import sys
+
+def get_request_id(url):
+    response = requests.get(url)
+    request_id = response.headers.get('X-Request-Id')
+    print(request_id)
+    return request_id
+
+url = sys.argv[1]
+    
+get_request_id(url)
